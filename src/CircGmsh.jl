d1f4819a4bd711b61299a454673ac69f917847ed
DanielBoigk <dn.boigk@gmail.com>
2024-04-25 00:44:31 +0200

    Geo File

--- src/CircGmsh.jl
+++ src/CircGmsh.jl
@@ -2,15 +2,28 @@
 
 export create_circle_geo
 
-function create_circle_geo(charlength::Float64=0.1)
-    all = "SetFactory(\"OpenCASCADE\");\n
-Circle(1) = {0, 0, 0, 1, 0, 2*Pi};\n
-Physical Surface(\"Circle\") = {1};\n
-Mesh.CharacteristicLengthMax = " * string(charlength) * ";  // Adjust mesh density\n
-Mesh 2;\n
-Save \"circle.msh\";"
+# This works with FerriteGmsh but not with GridapGmsh:
+function create_circle_geo(cellsize::Float64=0.05)
+    all = 
+    "cellSize = "*string(cellsize)*";
+    radius = 1;
+    Point(1) = {0, 0, 0, cellSize};
+    Point(2) = {-radius, 0, 0, cellSize};
+    Point(3) = {0, radius, 0, cellSize};
+    Point(4) = {radius, 0, 0, cellSize};
+    Point(5) = {0, -radius, 0, cellSize};
+    Circle(6) = {2, 1, 3};
+    Circle(7) = {3, 1, 4};
+    Circle(8) = {4, 1, 5};
+    Circle(9) = {5, 1, 2};
+    Line Loop(10) = {6, 7, 8, 9};
+    Plane Surface(11) = {10};
 
-write("circle.geo", all)
+    // Mark the boundary of the circular mesh
+    Physical Line(\"Boundary\") = {6, 7, 8, 9};
+    Physical Surface(\"Disk\") = {11};
+    // Mesh generation commands (if needed)
+    Mesh 2;"
 
+    write("circle.geo", all)
 end
-
