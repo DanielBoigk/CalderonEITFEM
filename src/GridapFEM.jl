e5f399a6631c3003c3b6f5295ff98569b0bccfb5
DanielBoigk <dn.boigk@gmail.com>
2024-04-29 01:30:59 +0200

    Circular Solution

--- src/GridapFEM.jl
+++ src/GridapFEM.jl
@@ -1,6 +1,7 @@
 # Here is the GRidap FEM solution
 
 export EIT_FEM_neumann_to_dirichlet, EIT_FEM_dirichlet_to_neumann
+export EIT_FEM_neumann_to_dirichlet_circ, EIT_FEM_dirichlet_to_neumann_circ
 
 
 # Receives two mxn Matrices with on describing σ on the domain and the other one describing the neumann boundary condition 
@@ -112,13 +113,37 @@ function EIT_FEM_dirichlet_to_neumann_circle(σ_function,j_function)
   @time begin
     uh = solve(op)
   end
+  return uh
+end
+
+function EIT_FEM_neumann_to_dirichlet(σ_function,j_function)
+
   @time begin
-    # Extract Matrix Valued solution:
-    Out = zeros(x_dim,y_dim)
-    S = uh.free_values
-    
-    S = reshape(uh.free_values, (x_dim-2,y_dim-2))
-    Out[2:x_dim-1,2:x_dim-1]=S
+     # Once known define this correctly:
+     domain = (-1.0, 1.0, -1.0, 1.0)
+
+     #Later: test if circle.msh already exists. If not run:
+     #run(`gmsh -2 circle.geo -o circle.msh`)  
+     model = GmshDiscreteModel("circle.msh")
+     reffe = ReferenceFE(lagrangian, Float64, 1)
+     V = TestFESpace(model, reffe, conformity=:H1)
+     U = V 
+
+  #Define Triangulation:
+  Ω = Triangulation(model)
+  dΩ = Measure(Ω,2)
+  Γ = BoundaryTriangulation(model)
+  dΓ = Measure(Γ,2) 
+
+  # Weak problem:
+  a(u, v) = ∫( σ_function * ∇(v) ⋅ ∇(u) )dΩ
+  l(v) = ∫( j_function * v )dΓ  # Neumann condition
+  
+  # And Solve
+  op = AffineFEOperator(a,l,U,V)
   end
-    return Out, uh
+  @time begin
+  uh = solve(op)
+  end
+  return uh
 end
