277a03d8ca5beb3f53c3eecb5027d9e9f5ec9f14
DanielBoigk <dn.boigk@gmail.com>
2024-05-24 02:48:49 +0200

    Boundary Reorder

--- src/FEMGridap/GridapGradient.jl
+++ src/FEMGridap/GridapGradient.jl
@@ -3,7 +3,8 @@
 
 #export Gradient_to_Normal #, gen_EIT_training_sqr
 
-export extract_gradient, extract_gradient_border
+export extract_gradient #, extract_gradient_border
+export gradient_atPoints
 #=
 function Gradient_to_Normal(G::Array{Float64, 3})
     n = size(G)[1]
@@ -26,9 +27,22 @@ end
 
 =#
 
+#Will extract gradient at given points:
+function gradient_atPoints(u, coordinates)
+    # This is slow af: improve when time
+    n = size(coordinates,1)
+    grid_points = [Point((coordinates[i,1], coordinates[i,2])) for i in 1:n]
+    grad_u= ∇(u)
+    # This is the really really bad part:
+    @time begin
+    Grid_Gradient = grad_u.(grid_points)
+    end
+    return [Grid_Gradient[i][j] for i in 1:n, j in 1:2]
+end
+
 
 # Will extract gradient given FEM solution u on domain [0,1]×[0,1]
-function extract_gradient(u, x_dim::Int64=100, y_dim::Int64=100)
+#=function extract_gradient(u, x_dim::Int64=100, y_dim::Int64=100)
     # This is slow af: improve when time
     x_range = range(0.0, stop=1.0, length=x_dim)
     y_range = range(0.0, stop=1.0, length=y_dim)
@@ -56,7 +70,7 @@ function extract_gradient_border(u, x_dim::Int64=100, y_dim::Int64=100)
     G = [Grid_Gradient[j][i] for j in 1:n, i in 1:2]
     return G
 end
-
+=#
 function extract_gradient(uh, points)
     grad_u = ∇(uh)
     grid_points = [Point( (points[i,1], points[i,2])) for i in 1:size(points,1)]
