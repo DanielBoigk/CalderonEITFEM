c521f39bcb76eec68aa55422d14923d14ba37c94
DanielBoigk <dn.boigk@gmail.com>
2025-03-13 02:53:10 +0100

    save somewhere bu discard

--- src/CalcData/FullProblem.jl
+++ src/CalcData/FullProblem.jl
@@ -1,5 +1,8 @@
 struct EITSolution # Ideally keep it immutable
     mesh    # Triangulation of the mesh
+    free_points::Int64
+    boundary_points::Int64
+    boundary_vals
     V_n_space
     U_n_space
     V_d_space
@@ -8,9 +11,11 @@ struct EITSolution # Ideally keep it immutable
     γ_vec # free values of conductivity
     K_n     # Stiffness matrix for Neumann problem
     K_d # Stiffness matrix for Dirichlet problem
-    G # Orthonormal basis for current pattern
-    F # Corresponding voltage measurements
-    singular # Singular values of NtD map
+    G::Dict # Orthonormal basis for current pattern
+    F::Dict # Corresponding voltage measurements
+    u_f::Dict # FEFunctions of voltage measurements
+    g_f::Dict # FEFunctions of current measurements
+    singular::AbstractVector # Singular values of NtD map
     NtD # NtD map
     DtN # DtN map (Moore-Penrose inverse of NtD)
     deleted_rows # What rows are to be deleted in F
